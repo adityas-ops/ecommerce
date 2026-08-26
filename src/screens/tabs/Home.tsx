48b7d6b4d18cba02fbaecc1d2bfebf3d8b072a47
Aditya Sharma <adityakushinagar123@gmail.com>
2026-08-26 12:29:50 +0530

    all api setup

--- src/screens/tabs/Home.tsx
+++ src/screens/tabs/Home.tsx
@@ -1,5 +1,7 @@
 import { View, Text } from 'react-native';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { getProducts } from '../../api/productApi';
+import { Product } from '../../types/product';
 
 const Home = () => {
   return (
